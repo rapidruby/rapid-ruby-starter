97527ba6036b0e3a48cbca1a72aa41d538b58389
Pete Hawkins <petey@hey.com>
2023-02-05 10:23:26 +0000

    Add styled header

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
 end
